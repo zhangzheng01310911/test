0ac465a7f6e96a1595eb5037611153d61ad938b6
zhangzheng01310911 <145755078+zhangzheng01310911@users.noreply.github.com>
2023-11-10 14:34:17 +0800

    Add files via upload

---
+++ table1-Ch.sh
@@ -0,0 +1,15 @@
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10
+
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10
+
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --GNNCONV GraphConv --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --GNNCONV GraphConv --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --GNNCONV GraphConv --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --GNNCONV GraphConv --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10
+
---
+++ table234_Ch_FW.sh
@@ -0,0 +1,57 @@
+# 10 
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CP --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+
+# 1
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE qr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+
+
+# 1
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+
+
+# 1
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN GAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN DiGAE --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GraphConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV SAGEConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GCNConv --Conditional True --Weighted False
+python3 main_all_in_one_conditional_Ch.py --SCORE cqr_new  --CP_or_CQR CQR --DiGAE_or_GAE_or_LGNN LGNN --SEEDNUM 10 --GNNCONV GATConv --Conditional True --Weighted False
